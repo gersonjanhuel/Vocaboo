08fe6ca6776ec6c708788cca90fd65299e22921b
Gerson Janhuel <gersonjanhuel@gmail.com>
2021-08-02 15:42:22 +0700

    Initial Commit

---
+++ Vocaboo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vocaboo
+//
+//  Created by Gerson Janhuel on 02/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vocaboo/VocabooApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabooApp.swift
+//  Vocaboo
+//
+//  Created by Gerson Janhuel on 02/08/21.
+//
+
+import SwiftUI
+
+@main
+struct VocabooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
